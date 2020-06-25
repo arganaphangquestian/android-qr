dd28c671790ac6f115fd6620c0fd15b22c0b5bb5
Argana Phangquestian <arganaphangquestian@gmail.com>
2020-06-25 22:12:30 +0700

    Initial commit :fire:

---
+++ app/src/main/java/dev/falcer/qrcode/MainActivity.kt
@@ -0,0 +1,28 @@
+package dev.falcer.qrcode
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mb_main.setOnClickListener {
+            IntentIntegrator(this).initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val result = IntentIntegrator.parseActivityResult(resultCode, data)
+        if(result != null) {
+            tv_result.text = result.contents
+            Toast.makeText(this.baseContext, result.contents, Toast.LENGTH_LONG).show()
+        }
+    }
+}
